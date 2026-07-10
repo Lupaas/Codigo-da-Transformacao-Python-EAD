7bd3d08ffbdbd46f00fa74dd0d7444ae181b24b2
Lupaas <luanepenafortns@gmail.com>
2026-07-09 23:11:36 -0300

    Update 2.0

---
+++ modulo02/modulo02_intro_python_luane-ativ02.py
@@ -0,0 +1,7 @@
+print("Olá, Mundo! Estou testando o Python.")
+print(42)
+
+
+print("O tipo de 'Olá, Mundo' é:", type("Olá, Mundo"))  
+print("O tipo de 42 é:", type(42))                     
+print("O tipo de 4.2 é:", type(4.2))                   
